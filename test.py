3c652ce9b96879c3d37f53e5cd0092fc3ff7867f
Marco160189 <86518629+Marco160189@users.noreply.github.com>
2021-06-26 13:35:22 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+
+
+
+package de.marco160189.mockme;
+
+
+public statik void switchOnCalculator() {
+		System.out.println("\tSwitch on calculator");
+		calculator = new Calculator();
+		calculator.switchOn();
+  @Calculator
+	public void multiply() {
+		calculator.add(2); 
+		calculator.multiply(2); 
+		assertEquals(calculator.getResult(), 20);
+
+                @Test(expected = ArithmeticException.class)
+	public void testJUnitMessage(){
+	}
+}
